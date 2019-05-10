fcd954872c7625bb9ac8039d26abb39e4c6b39cb
Isabella Torres <izzy612@gmail.com>
2019-05-10 02:13:29 -0400

    working progress

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (name)
+
+VALUES
+
+("New Bacon-ings"),
+("Mission A-Corn-Plished Burger"),
+("Something's NOT Fishy Burger - (100% Beef)"),
+("Poblano Picasso Burger");
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static("public"));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
+app.set('view engine', 'handlebars');
+
+
+require('')
+
+
+
+
