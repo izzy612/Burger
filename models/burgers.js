fe93704e6ccf730de0cbb38c1e32be9cc0abc33b
Isabella Torres <izzy612@gmail.com>
2019-05-10 11:17:25 -0400

    still on promises

--- models/burgers.js
+++ models/burgers.js
@@ -3,20 +3,20 @@ const connection = require('./connection');
 const findAll = () => {
 
   return new Promise((resolve, reject) => {
-     connection.query('SELECT * FROM burgers', function (err, burgerData) {
-      
+    connection.query('SELECT * FROM burgers', function (err, burgerData) {
+
       if (err) {
         return reject(err)
       }
-      
+
       return resolve(burgerData);
-    
-      });
+
     });
+  });
 };
 
 const findbyId = burgerId => {
-  
+
   return new Promise((resolve, reject) => {
     connection.query('SELECT * FROM burgers WHERE id = ?', [burgerId], function (err, burgerData) {
       if (err) {
@@ -26,4 +26,15 @@ const findbyId = burgerId => {
     });
 
   })
-}
+};
+
+const create = burgerDataObj => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM  burgers SET ?', [burgerDataObj], function (err, burgerData) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(burgerData);
+    });
+  });
+}; 
