9d07a94dcc057fff14c3d44eeed41937b5410c46
Isabella Torres <izzy612@gmail.com>
2019-05-13 11:19:33 -0400

    working on apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,5 +2,43 @@ const burgers = require("../models/burgers");
 
 module.exports = app => {
 
+  //Get all burgers API
+
+  app.get("/api/burgers", function (req, res) {
+    burgers.findAll()
+      .then(burgerData => res.json(burgerData))
+      .catch(err => {
+        console.log(err);
+        res.json(err);
+      });
+  });
+
+  //Post/create burgers Api
+
+  app.post("/apit/burgers", function (req, res) {
+    
+    burgers.create(req.body)
+      .then(burgerData => res.json(burgerData))
+      .catch(err => {
+        console.log(err);
+        res.json(err);
+    })
+  })
+
+  //Get burger by its id 
+  app.get("/api/burgers/:id", function (req, res) {
+    burgers.findbyId(req.params.id)
+      .then(burgerData => res.json(burgerData))
+      .catch(err => {
+        console.log(err);
+        res.json(err);
+      });
+  });
+
+  
+
+
+
+
 
 }
